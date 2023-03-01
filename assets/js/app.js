de2d8714495aa033be9f6cbfd8649d48b494f076
Brayan Barroso <brayanbarroso01@gmail.com>
2023-03-01 16:24:07 -0500

    ajustando menu a desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 const hamburguerIcon = document.querySelector('.nav__hamburguer');
 const navOverlay     = document.querySelector('.nav__overlay');
+let currentDropdown = navOverlay;
 
 // Evento para mostrar el menu en dispositivos moviles
 hamburguerIcon.addEventListener('click',()=>{
@@ -15,13 +16,17 @@ navOverlay.addEventListener('click',(e)=>{
     // console.log(e.target.classList.value);
     if(isActive(currentElement,'nav__parent')){
         const subMenu = currentElement.parentElement.children[1];
-        console.log(subMenu);
+        // console.log(subMenu);
         if(window.innerWidth < 768){
-let height = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
+        let height = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
 
-subMenu.style.height = `${height}px`;
+        subMenu.style.height = `${height}px`;
         }else{
-
+            if (!isActive(subMenu, 'nav__inner--show')) {
+                closeDropdown(currentDropdown);
+            }
+            subMenu.classList.toggle('nav__inner--show');
+            currentDropdown = subMenu;
         }
     }
 });
@@ -29,3 +34,20 @@ subMenu.style.height = `${height}px`;
 function isActive(element, string){
     return element.classList.value.includes(string);
 }
+
+function closeDropdown(currentDropdown){
+    if (isActive(currentDropdown, 'nav__inner--show')) {
+        currentDropdown.classList.remove('nav__inner--show');
+    }
+}
+
+window.addEventListener('resize', ()=>{
+    closeDropdown(currentDropdown);
+    
+    if (Window.innerWidth > 768) {
+        const navInners = document.querySelectorAll('.nav__inner');
+        
+        navInners.forEach(navInner =>{navInner.style.height = "";});
+        
+    }
+});
